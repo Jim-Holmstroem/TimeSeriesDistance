9f98075c32d9aacf42ded5e63be73fcc03b7c0fc
Jim Holmström <jim.holmstroem@gmail.com>
2013-10-07 08:48:17 +0200

    progress

--- timeseriesdistance/path.pyx
+++ timeseriesdistance/path.pyx
@@ -4,20 +4,26 @@ from operator import itemgetter
 from functools import partial
 from itertools import product, starmap
 
+from cython import boundscheck, wraparound
+
 import numpy as np
 cimport numpy as np
 
+@boundscheck(False)
+@wraparound(False)
 cdef inline lpc(
     np.ndarray[np.float64_t, ndim=2] D,
     Py_ssize_t i, Py_ssize_t j,
     double C_D, double C_HV
 ): # local path constraint
     return (
-        ((i-1, j-1), C_D *D[i-1, j-1]),
-        ((i  , j-1), C_HV*D[i  , j-1]),
-        ((i-1, j  ), C_HV*D[i-1, j  ]),
+        ((i - 1, j - 1), C_D  * D[i - 1, j - 1]),
+        ((i    , j - 1), C_HV * D[i    , j - 1]),
+        ((i - 1, j    ), C_HV * D[i - 1, j    ]),
     )
 
+@boundscheck(False)
+@wraparound(False)
 def D_from_c(
     np.ndarray[np.float64_t, ndim=2] c,
     double C_D, double C_HV
@@ -47,6 +53,8 @@ def D_from_c(
 
     return D
 
+@boundscheck(False)
+@wraparound(False)
 def min_path(
     np.ndarray[np.float64_t, ndim=2] D,
     double C_D, double C_HV
